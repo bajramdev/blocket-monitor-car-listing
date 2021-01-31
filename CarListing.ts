e9c23e1af190822561057f6a395242a35233dfb0
bajramdev <arfiesty0@gmail.com>
2021-01-31 01:03:24 +0100

    Found stock endpoint of specific car details and bear

---
+++ CarListing.ts
@@ -0,0 +1,9 @@
+export interface ICarListing {
+    name? : string
+    description? : string
+    gearBox? : string
+    milage? : number
+    modelYear? : number
+    horsePower? : number
+    price? : number
+}
